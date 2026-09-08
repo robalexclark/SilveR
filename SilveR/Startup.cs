636201730d76b20b70fedac381c8735ac13d8618
Robin Clark <robalexclark@hotmail.com>
2026-09-08 14:19:43 +0100

    Restore MVC JSON serialization

--- SilveR/Startup.cs
+++ SilveR/Startup.cs
@@ -70,7 +70,7 @@ namespace SilveR
 
             services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
